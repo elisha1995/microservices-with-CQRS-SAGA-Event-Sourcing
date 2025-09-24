798f3e39b579cfbc342b28899c2cf4148ce22216
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-09-24 23:42:04 +0000

    Add EventModel and EventStoreRepository for event sourcing implementation

---
+++ bank-account/account-cmd/src/main/java/com/techbank/account_cmd/domain/EventStoreRepository.java
@@ -0,0 +1,13 @@
+package com.techbank.account_cmd.domain;
+
+import com.techbank.cqrs_core.event.EventModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventStoreRepository extends MongoRepository<EventModel, String> {
+
+    List<EventModel> findByAggregateIdentifier(String aggregateIdentifier);
+}
