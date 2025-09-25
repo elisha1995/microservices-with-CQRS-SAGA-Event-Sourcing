185c6beb6b97330bf907ca31389560f71d0c9880
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-09-25 00:02:29 +0000

    Add EventSourcingHandler interface for event sourcing operations

---
+++ cqrs-es/cqrs-core/src/main/java/com/techbank/cqrs_core/handler/EventSourcingHandler.java
@@ -0,0 +1,8 @@
+package com.techbank.cqrs_core.handler;
+
+import com.techbank.cqrs_core.domain.AggregateRoot;
+
+public interface EventSourcingHandler<T> {
+    void save(AggregateRoot aggregate);
+    T getById(String id);
+}
